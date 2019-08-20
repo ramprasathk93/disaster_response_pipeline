6a39255ce7485cf8c68de2c5a04561eef401f1f8
ramprasathk93 <ramprasathk93@gmail.com>
2019-08-20 18:13:45 +1000

    include word cloud for messages

--- app/run.py
+++ app/run.py
@@ -2,13 +2,13 @@ import json
 import warnings
 import plotly
 import pandas as pd
-
+import numpy
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
+from wordcloud import WordCloud, STOPWORDS
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Scatter
 from sqlalchemy import create_engine
 from sklearn.externals import joblib
 warnings.filterwarnings('ignore')
@@ -44,7 +44,11 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    messages = df["message"]
+    text = tokenize(' '.join(messages))
+    wordcloud_json = plotly_wordcloud(text)
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -65,7 +69,8 @@ def index():
                     'title': "Genre"
                 }
             }
-        }
+        },
+        wordcloud_json
     ]
     
     # encode plotly graphs in JSON
@@ -93,6 +98,62 @@ def go():
         classification_result=classification_results
     )
 
+def plotly_wordcloud(text):
+    full_string = (" ").join(text)
+    wc = WordCloud(stopwords=set(STOPWORDS),
+                   max_words=200,
+                   max_font_size=100)
+    wc.generate(full_string)
+
+    word_list = []
+    freq_list = []
+    fontsize_list = []
+    position_list = []
+    orientation_list = []
+    color_list = []
+
+    for (word, freq), fontsize, position, orientation, color in wc.layout_:
+        word_list.append(word)
+        freq_list.append(freq)
+        fontsize_list.append(fontsize)
+        position_list.append(position)
+        orientation_list.append(orientation)
+        color_list.append(color)
+
+    # get the positions
+    x = []
+    y = []
+    for i in position_list:
+        x.append(i[0])
+        y.append(i[1])
+
+    # get the relative occurence frequencies
+    new_freq_list = []
+    for i in freq_list:
+        new_freq_list.append(i * 100)
+
+    plotly_json = {
+        'data': [
+            Scatter(
+                x=x,
+                y=y,
+                textfont=dict(size=new_freq_list,
+                              color=color_list),
+                hoverinfo='text',
+                hovertext=['{0}{1}'.format(w, f) for w, f in zip(word_list, freq_list)],
+                mode='text',
+                text=word_list
+            )
+        ],
+
+        'layout': {
+            'title': 'Word Cloud of frequent words',
+            'xaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False},
+            'yaxis': {'showgrid': False, 'showticklabels': False, 'zeroline': False}
+        }
+    }
+
+    return plotly_json
 
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
